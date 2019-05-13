0cda802f513c26a9e847917696a812a93d3b2282
Gaurav Gupta <gouravgupta840@gmail.com>
2019-05-14 00:39:14 +0530

    Understanding Evaluation Operators - expr

--- Read Operations - A Closer Look/index.js
+++ Read Operations - A Closer Look/index.js
@@ -98,3 +98,15 @@ use movieData
 db.movies.find({summary: "musical"}).pretty();
 
 db.movies.find({summary: {$regex: /musical/g }}).pretty();
+
+// Evaluation Operators - $expr
+
+use financialData
+
+db.sales.insertMany([{volume: 100, target: 120},{volume: 89, target: 80},{volume: 200, target: 177}])
+
+db.sales.find().pretty()
+
+db.sales.find({$expr: {$gt: ["$volume","$target"]}}).pretty()
+
+db.sales.find({$expr: {$gt: [{$cond: {if: {$gte: ["$volume", 190]}, then: {$subtract: ["$volume", 30]}, else: "$volume"}}, "$target" ]}}).pretty()
