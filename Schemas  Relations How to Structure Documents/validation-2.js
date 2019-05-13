18084a39364fcfced8de9476558653d4e4a29934
Gaurav Gupta <gouravgupta840@gmail.com>
2019-05-13 12:39:33 +0530

    Changing the Validation Action

---
+++ Schemas  Relations How to Structure Documents/validation-2.js
@@ -0,0 +1,42 @@
+db.runCommand ({
+  collMod: 'posts',
+  validator: {
+    $jsonSchema: {
+      bsonType: 'object',
+      required: ['title', 'text', 'creator', 'comments'],
+      properties: {
+        title: {
+          bsonType: 'string',
+          description: 'must be a string and is required',
+        },
+        text: {
+          bsonType: 'string',
+          description: 'must be a string and is required',
+        },
+        creator: {
+          bsonType: 'objectId',
+          description: 'must be a ObjectId and is required',
+        },
+        comments: {
+          bsonType: 'array',
+          description: 'must be a array and is required',
+          items: {
+            bsonType: 'object',
+            required: ['text', 'author'],
+            properties: {
+              text: {
+                bsonType: 'string',
+                description: 'must be a string and is required',
+              },
+              author: {
+                bsonType: 'objectId',
+                description: 'must be a objectId and is required',
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  validationAction: 'warn',
+});
