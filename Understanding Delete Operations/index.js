9ebeb84b37209f47278c51efcc06414d9927a2fc
Gaurav Gupta <gouravgupta840@gmail.com>
2019-05-15 13:05:03 +0530

    Deleting All Entries in a Collection

--- Understanding Delete Operations/index.js
+++ Understanding Delete Operations/index.js
@@ -7,3 +7,13 @@ db.users.deleteMany({ hobbies: { $elemMatch : {title: "Sports", frequency: 3} }}
 db.users.deleteMany({ totalAge: {$gt: 30}, hobbies: { $elemMatch : {title: "Sports", frequency: 3} }})
 
 db.users.deleteMany({ totalAge: {$exists: false}, isSporty: true })
+
+//  Delete All Entries in a Collection
+
+db.users.deleteMany({});
+
+db.users.drop()
+
+db.dropDatabase()
+
+show dbs
