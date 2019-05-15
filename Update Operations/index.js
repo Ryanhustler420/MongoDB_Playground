7b072c39b892a0c09a0b84c44b50024ab568fc68
Gaurav Gupta <gouravgupta840@gmail.com>
2019-05-15 11:46:00 +0530

    Updating All Array Elements

--- Update Operations/index.js
+++ Update Operations/index.js
@@ -52,3 +52,11 @@ db.users.updateOne({name: "Max"}, {$set: {name: "Boom", age: 29, hobbies: [{titl
 // Updating Matched Array Elements
 
 db.users.updateMany({"hobbies": {$elemMatch:{title:"Sports",frequency:{$gte: 3}}}},{$set: {"hobbies.$.highFrequency": true}})
+
+// Updatating All Array Elements
+
+db.users.find({"hobbies.frequency": {$gt: 2}}).pretty()
+
+db.users.updateMany({"hobbies.frequency": {$gt: 2}},{$set: {"hobbies.$.goodFrequency": true}}) // changes the first match
+
+db.users.updateMany({totalAge: {$gt: 30}},{$inc: {"hobbies.$[].frequency": -1}}) // update each element in array
