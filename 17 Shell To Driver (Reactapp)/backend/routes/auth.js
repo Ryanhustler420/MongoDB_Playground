202c2a3dfd0c3482578660138ee6d7639803dbc3
Gaurav Gupta <gouravgupta840@gmail.com>
2019-05-20 22:57:18 +0530

    Adding User Sign In

--- 17 Shell To Driver (Reactapp)/backend/routes/auth.js
+++ 17 Shell To Driver (Reactapp)/backend/routes/auth.js
@@ -14,12 +14,21 @@ router.post('/login', (req, res, next) => {
   const email = req.body.email;
   const pw = req.body.password;
   // Check if user login is valid
-  // If yes, create token and return it to client
-  const token = createToken();
-  // res.status(200).json({ token: token, user: { email: 'dummy@dummy.com' } });
-  res
-    .status(401)
-    .json({ message: 'Authentication failed, invalid username or password.' });
+  db.getDb().db().collection('users').findOne({email}).then(user => {
+    return bcrypt.compare(pw, user.password);
+  })
+  .then((result) => {
+    if(!result) {
+      throw Error();
+    }
+    const token = createToken();
+    res.status(200).json({ token: token, user: { email: email } });
+  })
+  .catch(err => {
+    res
+      .status(401)
+      .json({ message: 'Authentication failed, invalid username or password.' });
+  })
 });
 
 router.post('/signup', (req, res, next) => {
