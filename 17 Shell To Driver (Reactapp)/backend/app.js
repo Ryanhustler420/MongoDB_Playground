4fbbad90929fee652c66cd6aeb4170fdfeb10ae2
Gaurav Gupta <gouravgupta840@gmail.com>
2019-05-20 11:29:31 +0530

    Connecting Node.js  the MongoDB Cluster

--- 17 Shell To Driver (Reactapp)/backend/app.js
+++ 17 Shell To Driver (Reactapp)/backend/app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongodb = require('mongodb').MongoClient;
 
 const productRoutes = require('./routes/products');
 const authRoutes = require('./routes/auth');
@@ -24,5 +25,15 @@ app.use((req, res, next) => {
 
 app.use('/products', productRoutes);
 app.use('/', authRoutes);
+
+const uri = "mongodb+srv://<username>:<password>@cluster0-erk3k.mongodb.net/ReactShopDB?retryWrites=true";
+mongodb.connect(uri)
+  .then(client => {
+    console.log('Connected!');
+    client.close();
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 app.listen(3100);
